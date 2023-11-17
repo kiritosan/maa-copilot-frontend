9e1187d2752abcf4e835aa755c3744e0c4bcc177
willem <1121567132@qq.com>
2023-11-17 21:17:35 +0800

    fix: typo

--- src/components/NavExpandButton.tsx
+++ src/components/NavExpandButton.tsx
@@ -6,12 +6,12 @@ import { navAtom, toggleExpandNavAtom } from 'store/nav'
 
 export const NavExpandButton = () => {
   const expanded = useAtomValue(navAtom)
-  const toggleExpaned = useSetAtom(toggleExpandNavAtom)
+  const toggleExpand = useSetAtom(toggleExpandNavAtom)
 
   return (
     <div className="sm:hidden">
       <Button
-        onClick={toggleExpaned}
+        onClick={toggleExpand}
         icon={expanded.expanded ? 'cross' : 'menu'}
       />
     </div>
